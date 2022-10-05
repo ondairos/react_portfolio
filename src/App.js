04ca7b26ad3c1ef13145b461e3bd6ba57be120f8
Ondairos <ondairos@gmail.com>
2022-10-05 16:56:35 +0300

    testing stuff in app.js file

--- src/App.js
+++ src/App.js
@@ -26,6 +26,16 @@
 
 import { Component } from 'react';
 
+class RegularClass {}
+class ComponentClass extends Component {}
+
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+
+console.log('regularClassInstance', regularClassInstance);
+console.log('componentClassInstance', componentClassInstance);
+
+
 class App extends Component {
   render() {
     return (
