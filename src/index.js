84e1ee2c2b3fe172df475be63694ecb20c7a1cad
Ondairos <ondairos@gmail.com>
2022-10-05 16:51:32 +0300

    Testing classes in console on the server

--- src/index.js
+++ src/index.js
@@ -11,6 +11,42 @@ root.render(
   </React.StrictMode>
 );
 
+//test for class
+class Animal {
+  constructor(name,age) {
+    this.name =name;
+    this.age= age;
+  }
+
+  speak(){
+    console.log('I am ',this.name,'and, I am ', this.age,' years old');
+  }
+}
+
+const animal1 = new Animal ('Simba',3);
+animal1.speak();
+console.log(animal1);
+
+class Lion extends Animal {
+  constructor(name, age,furColor, speed){
+    super(name, age); //passes from the animal constructor
+    this.furColor = furColor;
+    this.speed = speed;
+  }
+
+  roar(){
+    console.log('ROAAAR I have',this.furColor," fur, and i ran at : ",this.speed, 'miles an hour!');
+  }
+}
+
+const lion1 = new Lion('Mufasa', 20,'golden',25);
+  lion1.speak();
+  lion1.roar();
+  console.log(lion1);
+
+
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
